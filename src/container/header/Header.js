f160863b0eb9beb956facde506026315a18c95b6
Tohir <toxa.tb9@gmail.com>
2021-05-25 18:29:06 +0600

    first step

--- src/container/header/Header.js
+++ src/container/header/Header.js
@@ -1,51 +1,87 @@
 import React from "react";
 import logoSvg from "../../images/logo.svg";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import NavbarLinkData from "./NavbarLinkData";
+import angleDown from "../../images/data- img-112.svg";
 const Header = () => {
   return (
     <header>
       <div className="container">
         <nav className="row navbar">
-          <div className="col-auto navbar-logo">
-            <a href="/">
+          <div className="col-auto">
+            <AnchorLink href="/home" className="navbar-logo" to="/" offset="72">
               <img src={logoSvg} alt="logo" />
-            </a>
+            </AnchorLink>
           </div>
           {/* <div className="col-auto">
             <button></button>
           </div> */}
-          <div className="col-auto">
+          <div className="col-auto navbar-menu">
             <ul className="navbar-items">
               <li className="navbar-item">
-                <a href="/" className="navbar-link">
+                <AnchorLink href="/" className="navbar-link">
                   Направления
-                </a>
+                </AnchorLink>
               </li>
               <li className="navbar-item">
-                <a href="/" className="navbar-link">
+                <AnchorLink href="/" className="navbar-link">
                   Команда
-                </a>
+                </AnchorLink>
               </li>
               <li className="navbar-item">
-                <a href="/" className="navbar-link">
-                  Услуги
-                </a>
+                <AnchorLink href="/" className="navbar-link">
+                  Услуги <img src={angleDown} alt="angel down" />
+                </AnchorLink>
+                <ul className="navbar-items-init">
+                  <p>Услуги</p>
+                  {NavbarLinkData[0].navItemInitOne.map(
+                    (linkInit, indexInit) => {
+                      return (
+                        <li className="navbar-item-init" key={indexInit}>
+                          <AnchorLink href="/" className="navbar-link-init">
+                            <img src={linkInit.img} alt={linkInit.text} />
+                            <span>{linkInit.text}</span>
+                          </AnchorLink>
+                        </li>
+                      );
+                    }
+                  )}
+                </ul>
               </li>
               <li className="navbar-item">
-                <a href="/" className="navbar-link">
+                <AnchorLink href="/" className="navbar-link">
                   Инструменты
-                </a>
+                </AnchorLink>
               </li>
               <li className="navbar-item">
-                <a href="/" className="navbar-link">
+                <AnchorLink href="/" className="navbar-link">
                   Клиенты
-                </a>
+                </AnchorLink>
               </li>
               <li className="navbar-item">
-                <a href="/" className="navbar-link">
-                  Портфолио
-                </a>
+                <AnchorLink href="/" className="navbar-link">
+                  Портфолио <img src={angleDown} alt="angle down" />
+                </AnchorLink>
+                <ul className="navbar-items-init">
+                  <p>Портфолио</p>
+                  {NavbarLinkData[0].navItemInitTwo.map(
+                    (linkInit, indexInit) => {
+                      return (
+                        <li className="navbar-item-init" key={indexInit}>
+                          <AnchorLink href="/" className="navbar-link-init">
+                            <img src={linkInit.img} alt={linkInit.text} />
+                            <span>{linkInit.text}</span>
+                          </AnchorLink>
+                        </li>
+                      );
+                    }
+                  )}
+                </ul>
               </li>
             </ul>
+            <div className="navbar-contact">
+              <AnchorLink href="/">Cвязаться</AnchorLink>
+            </div>
           </div>
         </nav>
       </div>
---
+++ src/container/header/NavbarLinkData.js
@@ -0,0 +1,27 @@
+import navbarImgOne_1 from "../../images/data- img-101.svg";
+import navbarImgOne_2 from "../../images/data- img-100.svg";
+import navbarImgOne_3 from "../../images/data- img-99.svg";
+import navbarImgOne_4 from "../../images/data- img-97.svg";
+import navbarImgOne_5 from "../../images/data- img-98.svg";
+import navbarImgTwo_1 from "../../images/data- img-106.svg";
+import navbarImgTwo_2 from "../../images/data- img-105.svg";
+import navbarImgTwo_3 from "../../images/data- img-104.svg";
+import navbarImgTwo_4 from "../../images/data- img-103.svg";
+
+export default [
+  {
+    navItemInitOne: [
+      { text: "Разработка мобильных приложений", img: navbarImgOne_1 },
+      { text: "Разработка и внедрение ERP систем", img: navbarImgOne_2 },
+      { text: "UI / UX дизайн", img: navbarImgOne_3 },
+      { text: "IT консалтинг", img: navbarImgOne_4 },
+      { text: "Оптимизация инфраструктуры", img: navbarImgOne_5 },
+    ],
+    navItemInitTwo: [
+      { text: "Delever", img: navbarImgTwo_1 },
+      { text: "Sms.uz", img: navbarImgTwo_2 },
+      { text: "Goodzone", img: navbarImgTwo_3 },
+      { text: "Iman", img: navbarImgTwo_4 },
+    ],
+  },
+];
